66c07dc864d9313ab8081db63027320ad5848b6c
Jailakwale <jai.techguy@gmail.com>
2022-04-12 11:25:33 +0200

    Project push

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Automatic training AWS',
+    author='DSTI',
+    license='BSD-3',
+)
